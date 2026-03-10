1cf54d08cd05f8cdf9ab1f6d405f04b6f290ef52
yangduheng <1064723285@qq.com>
2026-03-10 17:12:02 +0800

    修改

--- src/Counter.sol
+++ src/Counter.sol
@@ -10,11 +10,11 @@ contract Counter {
     }
 
     function increment() public {
-        number++;
+        number = number + 1;
     }
 
     function decrement() public {
-        number--;
+        number = number - 1;
     }
 
     function reset() public {
